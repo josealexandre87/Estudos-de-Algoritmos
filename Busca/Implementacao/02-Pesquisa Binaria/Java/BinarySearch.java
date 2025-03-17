5e1e9d8ded519803c96e4c87e771668a7b3d584f
josealexandre87 <jose.josealexandre87@gmail.com>
2025-03-17 18:25:54 -0300

    Adicionando implementação da pesquisa bináriar

--- Busca/Implementacao/02-Pesquisa Binaria/Java/BinarySearch.java
+++ Busca/Implementacao/02-Pesquisa Binaria/Java/BinarySearch.java
@@ -2,7 +2,30 @@
 
 public class BinarySearch {
 
-    }
+    public static int binarySearch(int[] arr, int target){
+        int left = 0; int right = arr.length -1;
+
+        while (left <= right){
+            int mid = left + (left + right) / 2; // Evita overflow em grandes números
+
+            if (arr[mid] == target){
+                return mid;
+            } else if(arr[mid] < target){
+                left = mid + 1;
+            } else {
+                right = mid -1;
+            }
+        }
+        return -1;
+    };
+
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 40;
+        int result = binarySearch(arr, target);
+        System.out.println(result != -1 ? "Elemento encontrado no índice: " + result : "Elemento não encontrado");
+    };
+};
 
 
 /* Execução: 
